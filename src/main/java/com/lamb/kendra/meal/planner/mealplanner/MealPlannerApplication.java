3373ad5f66c51097a8ec0469180f734b1098d357
klambo94@gmail.com <klambo94@gmail.com>
2018-12-14 10:37:39 -0600

    Initial Git commit

---
+++ src/main/java/com/lamb/kendra/meal/planner/mealplanner/MealPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.lamb.kendra.meal.planner.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealPlannerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lamb/kendra/meal/planner/mealplanner/MealPlannerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lamb.kendra.meal.planner.mealplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MealPlannerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
